62208fab744498a058241380a00943df0c8cf964
Alfonso Rognoni <alfonsorognoni@gmail.com>
2016-02-19 03:33:18 -0300

    estructura angular

---
+++ clase-1/app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clase1App
+ * @description
+ * # clase1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clase1App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
